9b9d8b49fc19a150f1904846650c28fd2ce9e4f4
黎明 <liming@addcn.com>
2018-08-29 08:23:20 +0800

    rebuilding site 2018-08-29 08:23:20

--- win10-mount-nfs.bat
+++ win10-mount-nfs.bat
@@ -1,4 +1,6 @@
 SET NFS_PATH="\\192.168.3.33\home\htdocs"
+SET MOUNT_PATH="Z:"
+SET USER="username"
 
 REM # 安装 NFS client
 REM #   dism 镜像路径 动作 动作参数
@@ -6,9 +8,7 @@ REM #   online 是指当前系统
 dism /online /Enable-Feature /FeatureName:ServicesForNFS-ClientOnly /FeatureName:ClientForNFS-Infrastructure /FeatureName:NFS-Administration /NoRestart
 
 
-REM # 修改匿名UID/GID
-REM #   dism 镜像路径 动作 动作参数
-REM #   online 是指当前系统
+REM # 修改默认的匿名UID/GID
 (
 echo Windows Registry Editor Version 5.00
 echo 
@@ -21,8 +21,8 @@ nfsadmin client stop
 nfsadmin client start
 
 
-
-mount -o anon -o nolock -o casesensitive=yes %NSF_PATH% Z:
+REM # 挂载到Z：
+runas /noprofile /user:%USER% mount -o anon -o nolock -o casesensitive=yes %NSF_PATH% %MOUNT_PATH%
 
 
 
